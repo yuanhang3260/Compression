2bc77ebc57ebd30e3480f7588861aedf6fa85ac0
hangyuan <yuanhang3260@gmail.com>
2014-12-18 01:00:28 -0500

    huffman tree created

--- src/Huffman/HuffmanTree.java
+++ src/Huffman/HuffmanTree.java
@@ -19,10 +19,15 @@ public class HuffmanTree implements Serializable {
      * Huffman Tree Node
      */   
     class TreeNode implements Comparable {
-        char encode;
-        TreeNode left;
-        TreeNode right;
+        byte b;
+        TreeNode left = null;
+        TreeNode right = null;
         int count;
+
+        public TreeNode(byte b, int count) {
+            this.b = b;
+            this.count = count;
+        }
 
         @Override
         public int compareTo(Object other){
@@ -52,16 +57,42 @@ public class HuffmanTree implements Serializable {
         try {
             BufferedInputStream ins = 
                 new BufferedInputStream(new FileInputStream(fileName)); 
-            byte[] buf = new byte[1024];  
-            int len = ins.read(buf, 0, buf.length);  
             
+            // count number of each byte
+            int[] counts = new int[256];
+            long len = file.length();
+            for (int i = 0; i < len; i++) {
+                byte b = (byte)ins.read();
+                counts[(int)(b&0x0FF)]++;
+            }
+
+            // create tree nodes and put them in a min heap
+            PriorityQueue<TreeNode> heap = new PriorityQueue<TreeNode>();
+            for (int i = 0; i < 256; i++) {
+                if (counts[i] > 0) {
+                    TreeNode node = new TreeNode((byte)(i&0xFF), counts[i]);
+                    heap.add(node);
+                }
+            }
+            nodeNum = heap.size();
+            
+            // create Huffman tree
+            for (int i = 0; i < nodeNum - 1; i++) {
+                TreeNode node1 = heap.poll();
+                TreeNode node2 = heap.poll();
+                TreeNode newNode = new TreeNode((byte)0, node1.count + node2.count);
+                newNode.left = node1;
+                newNode.right = node2;
+                heap.add(newNode);
+            }
+            this.root = heap.poll(); // final node is root
             ins.close();
         } 
         catch (IOException e) {
            e.printStackTrace();
         }
     }
-
+    
     /**
      * compressFile
      * @return compressed file size
